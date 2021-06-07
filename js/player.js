89f112394e02a06641275128f204c5f0ca0d1edd
belisards <adrianobf@gmail.com>
2021-06-07 19:39:11 -0300

    Update player.js

--- js/player.js
+++ js/player.js
@@ -21,7 +21,7 @@
 
   // URL da planilha
   // var sheetId = '1vnzlSBVUoT9aARJzQPNosx3eTPhFJNKr3G_G3XDV-w8';
-  var sheetId = '1VcZ4_8y3D9OaxnOpmkip9AmT_X8RY3BWJfXx8aBzVDs';
+  var sheetId = '1vnzlSBVUoT9aARJzQPNosx3eTPhFJNKr3G_G3XDV-w8';
 
   // URL da planilha convertida em JSON
   var sheetUrl = 'https://spreadsheets.google.com/feeds/list/' + sheetId + '/1/public/values?alt=json';
